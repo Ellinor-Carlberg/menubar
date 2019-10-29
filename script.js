4bfe67284debe0f3704efda73bcbb57fa0db3e51
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2019-10-29 11:14:45 +0100

    uppdated files

---
+++ script.js
@@ -0,0 +1,7 @@
+/* When the user clicks on the icon,
+toggle between hiding and showing the dropdown menu */
+function showDropDownMenu() {
+    document.getElementById(header).style.height = "100rem";
+  }
+
+
